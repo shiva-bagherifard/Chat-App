64b184d751ec85f2b2592f96c2469335bba6ee8d
shiva-bagherifard <shivabagheri1987@gmail.com>
2024-07-01 15:07:16 +0200

    implement AsyncStorage

--- components/Chat.js
+++ components/Chat.js
@@ -4,7 +4,7 @@ import { Bubble, GiftedChat } from "react-native-gifted-chat";
 import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({route, navigation, db}) => {
+const Chat = ({route, navigation, db, isConnected}) => {
   const { username, background, userID } = route.params;
     const [messages, setMessages] = useState([]);
     const onSend = (newMessages) => {
@@ -25,25 +25,57 @@ const Chat = ({route, navigation, db}) => {
         />
       }
 
-      // useEffect hook to set messages options
+      const renderInputToolbar = (props) => {
+        if (isConnected) return <InputToolbar {...props} />;
+        else return null;
+       }
+
+    // useEffect hook to set messages options
+    let unsubMessages;
     useEffect(() => {
+      if (isConnected === true){
+        // unregister current onSnapshot() listener to avoid registering multiple listeners when
+      // useEffect code is re-executed.
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
       navigation.setOptions({ title: username });
       // Create a query to get the "messages" collection from the Firestore database
       const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
       // This function will be called whenever there are changes in the collection.
-      const unsubMessages = onSnapshot(q, (docs) => {
+      unsubMessages = onSnapshot(q, (docs) => {
         let newMessages = [];
         // Iterate through each document in the snapshot
         docs.forEach(doc => {
           newMessages.push({ id: doc.id, ...doc.data(),  createdAt: new Date(doc.data().createdAt.toMillis()), })
         });
+        cacheMessages(newMessages);
         setMessages(newMessages);
       });
-
+    } else loadCachedMessages();
+     
+       // Clean up code
       return () => {
         if (unsubMessages) unsubMessages();
       }
-      }, []);
+    }, [isConnected]); //isConnected used as a dependency value enabling the component to call the callback of useEffect whenewer the isConnected prop's value changes.
+
+    const cacheMessages = async (messagesToCache) => {
+      try {
+        await AsyncStorage.setItem(
+          "messages",
+          JSON.stringify(messagesToCache)
+        );
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    // Call this function when isConnected prop turns out to be false in useEffect()
+    const loadCachedMessages = async () => {
+      // The empty array is for cachedMessages in case AsyncStorage() fails when the messages item hasn’t been set yet in AsyncStorage.
+      const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
+      setMessages(JSON.parse(cachedMessages));
+    };
 
       return (
         <View style={[styles.container, { backgroundColor: background }]}>
